db146701886ebe656195564e49502ca7214f2d6a
zeyad <zeyad@yahoo.com>
2022-11-26 23:02:12 +0200

    Added code files

---
+++ JavaExample.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaexample;
+
+/**
+ *
+ * @author Zeyad Medhat
+ */
+public class JavaExample {
+
+
+public static void main (String args [] ) {
+
+System.out.println(" Hi , Zeyad " );
+}
+}
+
